ea218186ff2c055710beeb48f0b191133cacc133
Chris Albright <calbright@cj.com>
2020-03-14 21:09:39 -0700

    add elasticsearch libs

--- spark-shell.sh
+++ spark-shell.sh
@@ -1,1 +1,8 @@
-docker run --rm -it -p 4040:4040 -v $(pwd)/COVID-19:/data -v $(pwd)/spark:/spark gettyimages/spark bin/spark-shell
+docker run --rm -it \
+-p 4040:4040 \
+--network covid-19-analysis_elastic-net \
+-v $(pwd)/COVID-19:/data \
+-v $(pwd)/spark:/spark \
+gettyimages/spark \
+bin/spark-shell \
+--packages org.elasticsearch:elasticsearch-hadoop:7.6.1
