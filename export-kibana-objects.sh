cbcb08aa60fbb64eb3d0a05d37b6dd08b02999c0
Chris Albright <calbright@cj.com>
2020-03-17 15:59:06 -0700

    rename kibana objects file

--- export-kibana-objects.sh
+++ export-kibana-objects.sh
@@ -2,5 +2,5 @@ curl -XPOST \
 -H 'Content-Type: application/json' \
 -H 'kbn-xsrf: true' \
 -d '{"type": ["canvas-element", "canvas-workpad", "config", "dashboard", "index-pattern", "lens", "map", "query", "search", "url", "visualization"]}' \
--o kibana-export.ndjson \
+-o kibana-objects.ndjson \
 http://localhost:5601/api/saved_objects/_export
