5aa7cfb0320f0eb441f933dc81b14574883627cf
Chris Albright <calbright@cj.com>
2020-03-17 15:51:32 -0700

    spark shell adds project jar

--- spark-shell.sh
+++ spark-shell.sh
@@ -2,7 +2,8 @@ docker run --rm -it \
 -p 4040:4040 \
 --network covid-19-analysis_elastic-net \
 -v $(pwd)/COVID-19:/data \
--v $(pwd)/spark:/spark \
+-v $(pwd)/target:/spark \
 gettyimages/spark \
 bin/spark-shell \
+--jars /spark/covid-19-analysis-0.1-SNAPSHOT.jar \
 --packages org.elasticsearch:elasticsearch-hadoop:7.6.1
