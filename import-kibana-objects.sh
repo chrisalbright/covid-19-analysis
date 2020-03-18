70b791b74d815bdcb72d6f6cde0f21b2935dec09
Chris Albright <calbright@cj.com>
2020-03-17 17:05:52 -0700

    overwrite saved objects

--- import-kibana-objects.sh
+++ import-kibana-objects.sh
@@ -1,4 +1,4 @@
 curl -X POST \
 -H "kbn-xsrf: true" \
 --form file=@kibana-objects.ndjson \
-http://localhost:5601/api/saved_objects/_import
+http://localhost:5601/api/saved_objects/_import?overwrite=true
